e458c2f096b428125534b0bf7acaa91b105ead9b
kazeem Adeniji <kazeemadeniji08@gmail.com>
2025-09-08 03:16:10 +0100

    removed url

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express()
 require('async-error-handler')
 const cookieParser = require('cookie-parser')
 
-const mongoose= require('mongoose')
 const morgan = require('morgan')
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
@@ -13,16 +12,11 @@ const taskRouter = require("./Routes/taskRoutes")
 
 app.use(express.json())
 app.use(morgan('dev'))
-// app.use(cookieParser(process.env.JWT_SECRET))
 app.use(cookieParser())
 
 app.use('/api/v1/tasks/auth', authRouter)
 app.use('/api/v1/tasks/users', userRouter)
 app.use('/api/v1/tasks', taskRouter)
 
-// mongoose
-// .connect(`mongodb+srv://Qasim:12345@tours.0uu0pza.mongodb.net/AUTH?retryWrites=true&w=majority`)
-// .then(console.log('db is connected'))
-// // .catch(console.log('db failed to connect'))
 connectDB()
 app.listen(2024, console.log('server is listening on port 2024'))
